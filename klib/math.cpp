c25e6cbd706febd2bc3d469d22eaa2daea8d5013
itzandroidtab <koen@itzandroidtab.nl>
2023-12-31 02:32:20 +0100

    added more math tests

--- klib/math.cpp
+++ klib/math.cpp
@@ -28,3 +28,44 @@ TEST_CASE("math 64 bit byte swap", "[klib::math]") {
     REQUIRE(klib::bswap64(0xaabb) == 0xbbaa000000000000);
     REQUIRE(klib::bswap64(0xdeadbeef00000000) == 0xefbeadde);
 }
+
+TEST_CASE("math count leading zeros", "[klib::math]") {
+    REQUIRE(klib::clz(0x00) == 32);
+    REQUIRE(klib::clz(0x01) == 31);
+    REQUIRE(klib::clz(0x80000000) == 0);
+    REQUIRE(klib::clz(0x8000) == 16);
+}
+
+TEST_CASE("math count trailing zeros", "[klib::math]") {
+    REQUIRE(klib::ctz(0x00) == 32);
+    REQUIRE(klib::ctz(0x01) == 0);
+    REQUIRE(klib::ctz(0x80000000) == 31);
+    REQUIRE(klib::ctz(0x8000) == 15);
+}
+
+TEST_CASE("math popcount", "[klib::math]") {
+    REQUIRE(klib::popcount(0x00) == 0);
+    REQUIRE(klib::popcount(0x01) == 1);
+    REQUIRE(klib::popcount(0x80000000) == 1);
+    REQUIRE(klib::popcount(0xffff) == 16);
+    REQUIRE(klib::popcount(0xffffffff) == 32);
+}
+
+TEST_CASE("math exp2", "[klib::math]") {
+    REQUIRE(klib::exp2(1) == (0x1 << 1));
+    REQUIRE(klib::exp2(32) == 0);
+    REQUIRE(klib::exp2(31) == (0x1 << 31));
+    REQUIRE(klib::exp2(15) == (0x1 << 15));
+    REQUIRE(klib::exp2<uint64_t>(63) == (static_cast<uint64_t>(0x1) << 63));
+}
+
+TEST_CASE("math pow", "[klib::math]") {
+    REQUIRE(klib::pow(2, 10) == klib::exp2(10));
+    REQUIRE(klib::pow(2, 0) == 1);
+    REQUIRE(klib::pow(2, 1) == 2);
+    REQUIRE(klib::pow(10, 5) == (10 * 10 * 10 * 10 * 10));
+}
+
+TEST_CASE("math ceil", "[klib::math]") {
+    REQUIRE(klib::ceil(9.5f) == 10.f);
+}
